d090f94f1d7b7d76b424abac00269d956cbfc6fc
730443 <730443@605-110-S13>
2023-04-04 08:45:09 -0600

    Add project files.

---
+++ For_loops___Spring_Break_Review.py
@@ -0,0 +1,11 @@
+for i in range(20, 51, 2):
+    print(i, end = " ")
+
+
+user = input("Type in 'bruh' for more ribbit's")
+while user == 'bruh':
+    print("ribbit!")
+    user = input("Or type 'frog' to stop")
+
+
+
